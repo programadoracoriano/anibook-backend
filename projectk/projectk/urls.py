8fd288222a9b15f10064adc5d3e69c153b46f149
Gualter Perinho <programadoracoriano@gmail.com>
2024-02-14 17:24:38 -0100

    adding readme

--- projectk/projectk/urls.py
+++ projectk/projectk/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
